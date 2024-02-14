ee2fcc4fa5018afd4896a7c46c0065dc6fd676a5
Bunje Kelly <kellybunje@gmail.com>
2024-02-14 10:10:53 +0000

    I was blind now i see lol

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""module used to manage API authentification"""
+
 from flask import request
 from typing import List, TypeVar
 
@@ -9,7 +11,7 @@ User = TypeVar('User')
 class Auth:
     """a class to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths"""
+        """public method that returns False - path and excluded_paths"""
         if path is None:
             return True
 
@@ -21,11 +23,11 @@ class Auth:
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
-        """returns None - request will be the Flask request object"""
+        """ method returns None - request will be the Flask request object"""
         if request is None:
             return None
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
-        """returns None - request will be the Flask request object"""
+        """method returns None - request will be the Flask request object"""
         return None
