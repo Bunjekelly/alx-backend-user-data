046dcb21c5cd68c58d0409c9c19c723ee0a5faca
Bunje Kelly <kellybunje@gmail.com>
2024-02-14 10:55:23 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,17 @@ class BasicAuth(Auth):
     def __init__(self) -> None:
         """method that initializes the class"""
         super().__init__()
+
+    @staticmethod
+    def extract_base64_authorization_header(authorization_header: str) -> str:
+        """a method that returns the Base64 part of the
+        Authorization header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        base64_auth = authorization_header.split('Basic ')[1]
+        return base64_auth
