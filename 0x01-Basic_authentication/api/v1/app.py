c7bbdddf55a0157690d445fd88d4e6f2bfd778d9
Bunje Kelly <kellybunje@gmail.com>
2024-02-14 09:36:53 +0000

    fixed pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,7 +30,9 @@ if AUTH_TYPE == 'basic_auth':
 def before_request_check():
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
@@ -56,6 +58,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """forbidden error handler"""
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
