429d51334a45e5ed32eb83a32256e5d3105e59d7
Bunje kelly <kellybunje@gmail.com>
2024-02-22 14:59:48 +0100

    quering users

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,13 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments"""
+        users = self._session.query(User)
+        try:
+            return users.filter_by(**kwargs).first()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
