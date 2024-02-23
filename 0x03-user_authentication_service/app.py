9ede1e13b3927df4f3a7e18ad9551e5a6b1b68a6
Bunje Kelly <kellybunje@gmail.com>
2024-02-23 11:22:21 +0000

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,11 +7,26 @@ from flask import Flask, abort, jsonify, redirect, request
 
 app = Flask(__name__)
 
+AUTH = Auth()
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """This the index function"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """Registers new users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
