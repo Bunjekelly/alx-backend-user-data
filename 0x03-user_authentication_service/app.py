4c95245dce9cc82ad9e3a2690ebe8867870a4502
Bunje Kelly <kellybunje@gmail.com>
2024-02-23 11:05:01 +0000

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,10 +7,12 @@ from flask import Flask, abort, jsonify, redirect, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """This the index function"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
